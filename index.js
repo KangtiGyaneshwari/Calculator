256e3fb7235eb790a4f1b4fda420311d4afa0963
KangtiGyaneshwari <138871696+KangtiGyaneshwari@users.noreply.github.com>
2023-07-12 23:10:39 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,129 @@
+var clickSound=new Audio() 
+clickSound.src="./click.mp3" 
+ 
+var clickSound1=new Audio() 
+clickSound1.src="./click1.mp3" 
+  
+var clickSound2=new Audio() 
+clickSound2.src="./click2.mp3" 
+ 
+var clickSound3=new Audio() 
+ clickSound3.src="./click3.mp3" 
+  
+ var clickSound4=new Audio() 
+ clickSound4.src="./click4.mp3" 
+ 
+var clickSound5=new Audio() 
+clickSound5.src="./click5.mp3" 
+ 
+var clickSound6=new Audio() 
+clickSound6.src="./click6.mp3" 
+ 
+var clickSound7=new Audio() 
+clickSound7.src="./click7.mp3" 
+ 
+var clickSound8=new Audio() 
+clickSound8.src="./click1.mp3" 
+ 
+var clickSound9=new Audio() 
+clickSound9.src="./click2.mp3" 
+ 
+var clickSound10=new Audio() 
+clickSound10.src="./click3.mp3" 
+ 
+var clickSound11=new Audio() 
+clickSound11.src="./click4.mp3" 
+ 
+var clickSound12=new Audio() 
+clickSound12.src="./click5.mp3" 
+ 
+var clickSound13=new Audio() 
+clickSound13.src="./click6.mp3" 
+ 
+var clickSound14=new Audio() 
+clickSound14.src="./click7.mp3" 
+ 
+var clickSound15=new Audio() 
+clickSound15.src="./click1.mp3" 
+ 
+var clickSound16=new Audio() 
+clickSound16.src="./1sec.mp3" 
+ 
+var clickSound17=new Audio() 
+clickSound17.src="./1sec.mp3" 
+ 
+
+ 
+function clickAudio() 
+{ 
+    clickSound.play() 
+} 
+function clickAudio1() 
+{ 
+    clickSound1.play() 
+} 
+function clickAudio2() 
+{ 
+    clickSound2.play() 
+} 
+function clickAudio3() 
+{ 
+    clickSound3.play() 
+} 
+function clickAudio4() 
+{ 
+    clickSound4.play() 
+} 
+function clickAudio5() 
+{ 
+    clickSound5.play() 
+} 
+function clickAudio6() 
+{ 
+    clickSound6.play() 
+} 
+function clickAudio7() 
+{ 
+    clickSound7.play() 
+} 
+function clickAudio8() 
+{ 
+    clickSound8.play() 
+} 
+function clickAudio9() 
+{ 
+    clickSound9.play() 
+} 
+function clickAudio10() 
+{ 
+    clickSound10.play() 
+} 
+function clickAudio11() 
+{ 
+    clickSound11.play() 
+} 
+function clickAudio12() 
+{ 
+    clickSound12.play() 
+} 
+function clickAudio13() 
+{ 
+    clickSound13.play() 
+} 
+function clickAudio14() 
+{ 
+    clickSound14.play() 
+} 
+function clickAudio15() 
+{ 
+    clickSound15.play() 
+} 
+ 
+function clickAudio16() 
+{ 
+    clickSound16.play()
+} 
+function clickAudio17() 
+{ 
+    clickSound17.play() 
+}
